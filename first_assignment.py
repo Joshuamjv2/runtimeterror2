5ae5bc11f3480076138fe0cad94c68accbcad50c
Joshua <joshuamjv22@gmail.com>
2020-03-04 15:33:02 -0800

    grading and weights

--- first_assignment.py
+++ first_assignment.py
@@ -12,3 +12,29 @@ marks = input('Respective Marks for main subjects in uppercase: ').split()
 subsidiary = input('Subsidiary subject: ')
 sub_mark = input('Sub-Aggregates: ')
 gp_marks = input('GP marks: ')
+
+#grading for marks
+grading = {'A':6, 'B':5, 'C':4, 'D':3, 'E':2, 'O':1, 'F':0}
+output = ''
+
+for ch in marks:
+    output+=str(grading.get(ch))
+
+a = output[0]
+b = output[1]
+c = output[2]
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+#total points
+Total_points = a + b + c
+
+#dealing with weights
+for_weights = [a, b, c]
+for_weights.sort(reverse=True)
+top_two_for_weights = (for_weights[0]+for_weights[1])*3
+last_for_weights = (for_weights[2])*2
+
+total_weights_main = top_two_for_weights + last_for_weights
